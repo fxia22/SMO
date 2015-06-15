7c871faf0801cf8a72ede699506e1a31944ec4b8
xf <xf1280@gmail.com>
2015-06-15 23:52:31 +0800

    update

--- SVDDmy.m
+++ SVDDmy.m
@@ -1,44 +1,49 @@
-function [alpha,mu] = SVDDmy(K, C, tol)
-m=size(K);
-alpha =zeros(m(1),1);
-alpha =alpha+1/m(2);
+function [R,alpha] = SVDDmy(K, C, tol)
+m=size(K);     %获取元素个数
+alpha =rand(m(1),1);
+alpha =alpha/sum(alpha);  %初始化alpha
 ntp=m(1);
+error=zeros(ntp,1);
 for i = 1:ntp
-    error(i) =1-2/m(1) *sum(K(i,:));
+    error(i) =K(i,i)-2*alpha'*K(i,:)';       %计算初始的error
 end
-iter=0;
 while 1
-    POS=find(alpha>0);
-    MIN=min(error(POS));
-     POS1=find(alpha<C);
-    MAX=max(error(POS1));
-  
+    POS1=find(alpha>0);          
+    MIN=min(error(POS1));           %寻找能减少的alpha中error最小的
+     POS2=find(alpha<C);           
+    MAX=max(error(POS2));         %寻找能增加的alpha中error最大的
+    if(MIN>MAX)
+        break;
+    end
     i1=find(error==MIN);
     i2=find(error== MAX);
-    if(size(i1,2)>1)
+    if(size(i1,1)>1)              %若找到多个取第一个
         i1=i1(1);
     end
-     if(size(i2,2)>1)
+     if(size(i2,1)>1)
         i2=i2(1);
     end
     disp(num2str(size(i1)));
-    disp(num2str(size(i2)));
+    disp(num2str(size(i2)));     
     fprintf('%d   %d  %f  %f\n',i1,i2,MAX,MIN);
-    if(MIN+tol>=MAX)
-        break;
-    end
-    lamda=min([C-alpha(i2),alpha(i1),( error(i2)- error(i1))/2/(K(i1,i1)+K(i2,i2)-2*K(i1,i2))]);
+    lamda=min([C-alpha(i2),alpha(i1),( error(i2)- error(i1))/2/(K(i1,i1)+K(i2,i2)-2*K(i1,i2))]);        %计算λ
     for i = 1:ntp
-    error(i) =error(i)-2*lamda*(K(i2,i)-K(i1,i));
+    error(i) =error(i)-2*lamda*(K(i2,i)-K(i1,i));              %迭代更新error
     end
-    iter=iter+1;
     alpha(i2)=alpha(i2)+lamda;
     alpha(i1)=alpha(i1)-lamda;
-    out(iter)=alpha'*diag(K)-alpha'*K*alpha;
-     fprintf('%f %f',out(iter),lamda); 
-    
+     if(lamda<tol)
+        break;
+    end
 end
-
-    mu=0;
+R=0;
+iter=0;
+for i=1:ntp
+    if(alpha(i)>0&&alpha(i)<C)
+        R=R+K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha;          %利用支持向量计算R
+        iter=iter+1;
+    end
+end
+R=R/iter;
     
 end
--- testsvdd.m
+++ testsvdd.m
@@ -1,16 +1,71 @@
-data = importdata('iris.data2');
-numdata = data;
-y = numdata(:,5);
-n = size(y,1);
+sigama=2;
+C=0.05;
 
-X = numdata(:,1:4);
 
-K = zeros(n,n);
+%r=find(cancel(:,10)==1);
+
+%r=find(y==0);
+X=banana;
+%X=X_new(r,:);
+n=size(X,1);
+for i=1:2
+    Xstd(i)=std(X(:,i));
+    if(Xstd(i)==0)
+        return;
+    end
+    Xmean(i)=mean(X(:,i));
+    X(:,i)= (X(:,i)-Xmean(i))/Xstd(i);
+end
+K = zeros(n,n); 
 for i = 1:n
     for j = 1:n
-        K(i,j) = exp(-norm(X(i,:) - X(j,:))/10);
-        %K(i,j) = X(i,:)*X(j,:)';
+       K(i,j) = exp(-(norm(X(i,:) - X(j,:)))/sigama);
+        
+       % K(i,j) = (X(i,:)*X(j,:)');
     end
 end
 
-[alpha,mu] = svdd(K,y',0.25,0.00001);
+[R,alpha] = SVDDmy(K,C,0.0000001);
+
+cor1=0;
+cor2=0;
+for i=1:n
+    if(alpha(i)==0)
+      if(K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha<=R)  
+          cor1=cor1+1;
+      end
+    end
+    if(alpha(i)==C)
+        tt=K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha;
+      if(K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha>R)  
+          cor2=cor2+1;
+      end
+    end
+    if(alpha(i)<C&&alpha(i)>0)
+        tt=K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha;
+      if(K(i,i)-2*alpha'*K(:,i)+alpha'*K*alpha<=R)  
+          cor1=cor1+1;
+      end
+    end
+end
+r=find(alpha<C);
+cor1/size(r,1)
+cor2/(n-size(r,1))
+figure;
+plot(X(:,1),X(:,2),'.');
+hold;
+r=find(alpha>0);
+plot(X(r,1),X(r,2),'+');
+r=find(alpha==C);
+plot(X(r,1),X(r,2),'*');
+
+for i=1:650
+   tt(i)=1;
+   for j=1:n
+       tt(i)=tt(i)-2*alpha(j)*exp(-(norm(test1(i,:) - X(j,:)))/sigama);
+   end
+    tt(i)=tt(i)+alpha'*K*alpha;
+
+end
+r=find(tt<=R);
+plot(test1(r,1),test1(r,2),'*');
